dad0c995a340eb66f32ccd8409b0a822e65c05dc
xavelo <xavelo@gmail.com>
2024-07-01 09:35:39 +0200

    first commit

---
+++ src/main/java/com/xavelo/kafkaproducerk3s/TemplateAppController.java
@@ -0,0 +1,21 @@
+package com.xavelo.kafkaproducerk3s;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TemplateAppController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TemplateAppController.class);
+
+    @GetMapping("/ping")
+    public String ping() {
+        logger.info("ping received");
+        logger.info("pong returned");
+        return "pooooong";
+    }
+
+}
+
